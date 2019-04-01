451d70009ab9db02d31ba8880df7a94c89e1be80
eileenkit <46938819+eileenkit@users.noreply.github.com>
2019-03-31 20:54:47 -0600

    Add files via upload

---
+++ wk9/script.js
@@ -0,0 +1,15 @@
+var llamaFacts = new Vue({
+	el: '#llamaFacts',
+	data: {
+		llama1:"llama poop has almost no odor.",
+		llama2:"llamas don't bite.",
+		llama3:"llamas spit when they're agitated.",
+		llama4:"a group of llamas is called a herd.",
+		llama5:"a baby llama is called a 'cria'.",
+		llama6:"llamas live to be about 20 years old.",
+		llama7:"llamas are vegetarians.",
+		llama8:"llamas can grow to be 6 feet tall.",
+		llama9:"llamas are members of the camelid family.",
+		llama10:true,
+	}
+})
