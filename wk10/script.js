d983492069dc57f279307652080c71c875578e7e
eileenkit <46938819+eileenkit@users.noreply.github.com>
2019-04-07 20:28:05 -0600

    Add files via upload

---
+++ wk10/script.js
@@ -0,0 +1,42 @@
+var myVue = new Vue({
+      el: "#app",
+      data: {
+        quarters: [
+          {
+           name:"Delaware",
+           year:"January 1, 1999",
+           icon:"Caeser Rodney on horseback",
+           engraver:"William Cousins",
+           image: "images/delaware.png"
+          },
+          {
+           name:"Pennsylvania",
+           year:"March 8, 1999",
+           icon:"Commonwealth statue",
+           engraver:"John Mercanti",
+           image: 'images/pennsylvania.png'
+          },
+          {
+           name:"New Jersey",
+           year:"May 17, 1999",
+           icon:"Washington crossing the Delaware",
+           engraver:"Alfred Maletsky",
+           image: 'images/newjersey.png'
+          },
+          {
+           name:"Georgia",
+           year:"July 19, 1999",
+           icon:"A peach",
+           engraver:"T. James Ferrell",
+           image: 'images/georgia.png'
+          },
+          {
+           name:"Connecticut",
+           year:"October 12, 1999",
+           icon:"Charter Oak",
+           engraver:"T. James Ferrell",
+           image: 'images/connecticut.png'
+          },
+        ]
+      }
+    });
